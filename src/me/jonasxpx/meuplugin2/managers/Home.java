a45047e48b174dddacaa615fe7ed9596e5d2d499
jonas <jonas@DESKTOP-G3582GG>
2016-01-31 23:34:19 -0200

    Java 8 Update

--- src/me/jonasxpx/meuplugin2/managers/Home.java
+++ src/me/jonasxpx/meuplugin2/managers/Home.java
@@ -18,59 +18,47 @@ public class Home {
 
 	public void setHome(final String name){
 		final Location loc = player.getLocation();
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				int v;
-				if((v = homeSql.update(player, name, loc)) == 0){
-					homeSql.create(player, name, loc);
-					player.sendMessage("§6Você marcou uma nova posição: §6" + name);
-				}else
-					player.sendMessage("§6Você remarcou uma nova posição: §6" + name);
-			}
+		new Thread((Runnable) () -> {
+			int v = 0;
+			if((v = homeSql.update(player, name, loc)) == 0){
+				homeSql.create(player, name, loc);
+				player.sendMessage("§6Você marcou uma nova posição: §6" + name);
+			}else
+				player.sendMessage("§6Você remarcou uma nova posição: §6" + name);
 		}, "home - " + name + " : " + player.getName()).start();
 	}
 	
 	public void teleport(final String name){
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				String loc;
-				if((loc = homeSql.getStringLocation(player.getName(), name)) != null){
-					String locArray[] = loc.split(",");
-					player.sendMessage(locArray.length + "");
-					player.teleport(new Location(Bukkit.getWorld(locArray[5]), Double.parseDouble(locArray[0]), Double.parseDouble(locArray[1]), Double.parseDouble(locArray[2]), Float.parseFloat(locArray[3]), Float.parseFloat(locArray[4])));
-					player.sendMessage("§bVocê foi teleportado para §6" + name.toLowerCase());
-					return;
-				}
-				player.sendMessage("§cVocê não possui um local marcado com esse nome.");
+		new Thread((Runnable) () -> {
+			String loc;
+			if((loc = homeSql.getStringLocation(player.getName(), name)) != null){
+				String locArray[] = loc.split(",");
+				player.sendMessage(locArray.length + "");
+				player.teleport(new Location(Bukkit.getWorld(locArray[5]), Double.parseDouble(locArray[0]), Double.parseDouble(locArray[1]), Double.parseDouble(locArray[2]), Float.parseFloat(locArray[3]), Float.parseFloat(locArray[4])));
+				player.sendMessage("§bVocê foi teleportado para §6" + name.toLowerCase());
+				return;
 			}
+			player.sendMessage("§cVocê não possui um local marcado com esse nome.");
 		}, "home - " + name + " : " + player.getName()).start();
 	}
 	
 	public void listHomes(){
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				String listhome = null;
-				if((listhome = homeSql.listHomes(player.getName())) != null){
-					player.sendMessage(listhome);
-					return;
-				}
-				player.sendMessage("§cVocê não tem nenhuma home marcada");
+		new Thread(() -> {
+			String listhome = null;
+			if((listhome = homeSql.listHomes(player.getName())) != null){
+				player.sendMessage(listhome);
+				return;
 			}
+			player.sendMessage("§cVocê não tem nenhuma home marcada");
 		}).start();
 	}
 	
 	public void deleteHome(final String name){
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				if(homeSql.delete(player.getName(), name))
-					player.sendMessage("§bVocê deletou uma posição: §6" + name);
-				else
-					player.sendMessage("§bNão foi possível deletar essa posição.");
-			}
+		new Thread((Runnable) () -> {
+			if(homeSql.delete(player.getName(), name))
+				player.sendMessage("§bVocê deletou uma posição: §6" + name);
+			else
+				player.sendMessage("§bNão foi possível deletar essa posição.");
 		}, "home - " + name + " : " + player.getName()).start();
 	}
 
